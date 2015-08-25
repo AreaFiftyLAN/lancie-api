d9c658e22c842557b60d89affaf5e56305ad2517
Sille <sille.kamoen@gmail.com>
2015-08-25 14:50:46 +0200

    Implemented Sponspor resource

--- src/main/java/ch/wisv/areafiftylan/web/controller/WebRestController.java
+++ src/main/java/ch/wisv/areafiftylan/web/controller/WebRestController.java
@@ -2,8 +2,10 @@ package ch.wisv.areafiftylan.web.controller;
 
 import ch.wisv.areafiftylan.web.model.CommitteeMember;
 import ch.wisv.areafiftylan.web.model.Event;
+import ch.wisv.areafiftylan.web.model.Sponsor;
 import ch.wisv.areafiftylan.web.model.Tournament;
 import ch.wisv.areafiftylan.web.service.EventServiceImpl;
+import ch.wisv.areafiftylan.web.service.SponsorServiceImpl;
 import ch.wisv.areafiftylan.web.service.TournamentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +20,14 @@ public class WebRestController {
 
     TournamentServiceImpl tournamentService;
     EventServiceImpl eventService;
+    SponsorServiceImpl sponsorService;
 
     @Autowired
-    public WebRestController(TournamentServiceImpl tournamentService, EventServiceImpl eventService) {
+    public WebRestController(TournamentServiceImpl tournamentService, EventServiceImpl eventService,
+                             SponsorServiceImpl sponsorService) {
         this.tournamentService = tournamentService;
         this.eventService = eventService;
+        this.sponsorService = sponsorService;
     }
 
     @RequestMapping("/tournaments")
@@ -35,13 +40,23 @@ public class WebRestController {
         return eventService.getAllEvents();
     }
 
+    /**
+     * Returns all committee members. This is done in the Controller because it's simple and static nature
+     *
+     * @return A collection with all committee members.
+     */
     @RequestMapping("/committee")
     public Collection<CommitteeMember> getCommittee(){
         Collection<CommitteeMember> committeeMembers = new ArrayList<>();
         committeeMembers.add(new CommitteeMember("Sille Kamoen", "Chairman", "people"));
         committeeMembers.add(new CommitteeMember("Rebecca Glans", "Secretary", "women"));
-        committeeMembers.add(new CommitteeMember("Sven Popping", "Treasurere", "money"));
+        committeeMembers.add(new CommitteeMember("Sven Popping", "Treasurer", "money"));
 
         return committeeMembers;
+    }
+
+    @RequestMapping("/sponsors")
+    public Collection<Sponsor> getSponspors() {
+        return sponsorService.getAllSponsors();
     }
 }
