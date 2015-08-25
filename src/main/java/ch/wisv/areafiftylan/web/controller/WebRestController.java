52e06c7c149f2946935049c7088dafa49371e72c
Sille <sille.kamoen@gmail.com>
2015-08-25 14:29:39 +0200

    Implemented Events resource

--- src/main/java/ch/wisv/areafiftylan/web/controller/WebRestController.java
+++ src/main/java/ch/wisv/areafiftylan/web/controller/WebRestController.java
@@ -1,6 +1,8 @@
 package ch.wisv.areafiftylan.web.controller;
 
+import ch.wisv.areafiftylan.web.model.Event;
 import ch.wisv.areafiftylan.web.model.Tournament;
+import ch.wisv.areafiftylan.web.service.EventServiceImpl;
 import ch.wisv.areafiftylan.web.service.TournamentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,22 @@ import java.util.Collection;
 @RequestMapping("/web")
 public class WebRestController {
 
-    TournamentServiceImpl tournamentServiceImpl;
+    TournamentServiceImpl tournamentService;
+    EventServiceImpl eventService;
 
     @Autowired
-    public WebRestController(TournamentServiceImpl tournamentServiceImpl) {
-        this.tournamentServiceImpl = tournamentServiceImpl;
+    public WebRestController(TournamentServiceImpl tournamentService, EventServiceImpl eventService) {
+        this.tournamentService = tournamentService;
+        this.eventService = eventService;
     }
 
     @RequestMapping("/tournaments")
-    public Collection<Tournament> getAllTournamentsFromJsonFile() {
-        return tournamentServiceImpl.getAllTournaments();
+    public Collection<Tournament> getAllTournaments() {
+        return tournamentService.getAllTournaments();
+    }
+
+    @RequestMapping("/events")
+    public Collection<Event> getAllEvents() {
+        return eventService.getAllEvents();
     }
 }
