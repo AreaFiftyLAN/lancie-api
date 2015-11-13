a8927ce16160568bf939bcde9757de7b9bd504b4
Sille <sille.kamoen@gmail.com>
2015-11-13 13:14:14 +0100

    Updated CurrentUserService to allow for String principal
    Also removed unneeded OAuth dependency from build.gradle

--- src/main/java/ch/wisv/areafiftylan/service/CurrentUserService.java
+++ src/main/java/ch/wisv/areafiftylan/service/CurrentUserService.java
@@ -1,10 +1,8 @@
 package ch.wisv.areafiftylan.service;
 
-import org.springframework.security.core.userdetails.UserDetails;
-
 /**
  * The CurrentUserService is for permissions that require some logic to be determined.
  */
 public interface CurrentUserService {
-    boolean canAccessUser(UserDetails currentUser, Long userId);
+    boolean canAccessUser(Object principal, Long userId);
 }
