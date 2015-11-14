9594455e73a2886b557e4a6f1fdb03026ff693d9
Sille <sille.kamoen@gmail.com>
2015-11-14 16:45:13 +0100

    Add View documentation

--- src/main/java/ch/wisv/areafiftylan/model/view/View.java
+++ src/main/java/ch/wisv/areafiftylan/model/view/View.java
@@ -1,8 +1,23 @@
 package ch.wisv.areafiftylan.model.view;
 
 /**
- * Created by sille on 14-11-15.
+ * This class is responsible for hiding information in the JSON responses from Controllers. It's just an empty class
+ * with a few interfaces which define the level of visibility. In your model, if you mark a field with
+ *
+ * @JSONView([view]), only those fields will be displayed if you ALSO mark the returning Controller method with the same
+ * annotation.
+ * <p>
+ * View interfaces can be extended. Controllers returning this view will then also return their parent view. Use this to
+ * create visibilty levels.
  */
 public class View {
-        public interface Public {}
+    /**
+     * The Public view only displays fields that are truly public so that no private information is shown
+     */
+    public interface Public {}
+
+    /**
+     * The participant view shows information only accessible by event participants, such as seating information
+     */
+    public interface Participants extends Public {}
 }
