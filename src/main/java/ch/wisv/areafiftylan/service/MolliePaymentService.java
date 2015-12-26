e1e688bb4adcd68f077045583b94518184771d87
Sille <sille.kamoen@gmail.com>
2015-12-26 22:14:09 +0100

    Renamed PaymentServiceImpl to MolliePaymentService and cleared up structure

--- src/main/java/ch/wisv/areafiftylan/service/MolliePaymentService.java
+++ src/main/java/ch/wisv/areafiftylan/service/MolliePaymentService.java
@@ -25,7 +25,7 @@ import java.util.Map;
  * Created by sille on 25-12-15.
  */
 @Service
-public class PaymentServiceImpl implements PaymentService {
+public class MolliePaymentService implements PaymentService {
 
     OrderRepository orderRepository;
 
@@ -37,7 +37,7 @@ public class PaymentServiceImpl implements PaymentService {
     String method = "ideal";
 
     @Autowired
-    public PaymentServiceImpl(OrderRepository orderRepository, @Value("${a5l.molliekey}") String apiKey){
+    public MolliePaymentService(OrderRepository orderRepository, @Value("${a5l.molliekey}") String apiKey) {
         this.orderRepository = orderRepository;
         this.mollie = new ClientBuilder().withApiKey(apiKey).build();
     }
@@ -50,30 +50,55 @@ public class PaymentServiceImpl implements PaymentService {
 
         CreatePayment payment =
                 new CreatePayment(method, order.getPrice(), "Area FiftyLAN Ticket", returnUrl, metadata);
+
+        //First try is for IOExceptions coming from the Mollie Client.
         try {
+            // Create the payment over at Mollie
             ResponseOrError<CreatedPayment> molliePayment = mollie.createPayment(payment);
+
             if (molliePayment.getSuccess()) {
+                // All good, update the order
+                // Insert the Mollie ID for future reference
                 order.setReference(molliePayment.getData().getId());
                 order.setStatus(OrderStatus.WAITING);
+
+                // Save the changes to the order
                 orderRepository.save(order);
+
+                // Return the payment URL so the User can pay.
                 return molliePayment.getData().getLinks().getPaymentUrl();
             } else {
+                // Some error occured, but connection to Mollie succeeded, which means they have something to say.
                 ErrorData molliePaymentError = molliePayment.getError();
+
+                // Make the compiler shut up, this is something stupid in the Mollie API Client
+                @SuppressWarnings("unchecked")
                 HashMap<String, Object> errorMap = (HashMap<String, Object>) molliePaymentError.get("error");
+                //TODO: This should be a BAD REQUEST exception
                 throw new PaymentException((String) errorMap.get("message"));
             }
         } catch (IOException e) {
+            // This indicates the HttpClient encountered some error
+            // TODO: Make this throw a different Status 500 Exception.
             throw new PaymentException("Something went wrong requesting the payment", e.getCause());
         }
     }
 
     @Override
     public Order updateStatus(String orderReference) {
+        // TODO: Make this throw a proper BAD REQUEST exception
         Order order = orderRepository.findByReference(orderReference)
                 .orElseThrow(() -> new UserNotFoundException("Order with reference " + orderReference + " not found"));
+
+        // This try is for the Mollie API internal HttpClient
         try {
+            // Request a payment from Mollie
             ResponseOrError<PaymentStatus> molliePaymentStatus = mollie.getPaymentStatus(apiKey, orderReference);
+
+            // If the request was a success, we can update the order
             if (molliePaymentStatus.getSuccess()) {
+                // There are a couple of possible statuses. Enum would have been nice. We select a couple of relevant
+                // statuses to translate to our own status.
                 switch (molliePaymentStatus.getData().getStatus()) {
                     case "cancelled": {
                         order.setStatus(OrderStatus.EXPIRED);
@@ -84,13 +109,26 @@ public class PaymentServiceImpl implements PaymentService {
                     case "paid": {
                         order.setStatus(OrderStatus.PAID);
                     }
+                    case "paidout": {
+                        order.setStatus(OrderStatus.PAID);
+                    }
                 }
-                order = orderRepository.save(order);
+                return orderRepository.save(order);
+            } else {
+                // Some error occured, but connection to Mollie succeeded, which means they have something to say.
+                ErrorData molliePaymentError = molliePaymentStatus.getError();
+
+                // Make the compiler shut up, this is something stupid in the Mollie API Client
+                @SuppressWarnings("unchecked") HashMap<String, Object> errorMap =
+                        (HashMap<String, Object>) molliePaymentError.get("error");
+                //TODO: This should be a BAD REQUEST exception
+                throw new PaymentException((String) errorMap.get("message"));
             }
         } catch (IOException e) {
+            // This indicates the HttpClient encountered some error
+            // TODO: Make this throw a different Status 500 Exception.
             throw new PaymentException(e.getMessage());
         }
-        return order;
     }
 
     @Override
