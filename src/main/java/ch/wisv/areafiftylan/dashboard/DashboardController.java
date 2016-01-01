914d9173676daaea6e50b0f43d68f8f5b9ce91c5
Sille Kamoen <sille.kamoen@gmail.com>
2016-01-01 18:52:15 +0100

    Added User table to users overview

--- src/main/java/ch/wisv/areafiftylan/dashboard/DashboardController.java
+++ src/main/java/ch/wisv/areafiftylan/dashboard/DashboardController.java
@@ -1,34 +1,45 @@
 package ch.wisv.areafiftylan.dashboard;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import ch.wisv.areafiftylan.model.User;
+import ch.wisv.areafiftylan.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
-import static ch.wisv.areafiftylan.util.ResponseEntityBuilder.createResponseEntity;
+import java.util.Collection;
 
 @Controller
 @RequestMapping(value = "/dashboard")
 @PreAuthorize("hasRole('ADMIN')")
 public class DashboardController {
 
+    UserService userService;
+
+    @Autowired
+    public DashboardController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public String getIndexPage(){
+    public String getIndexPage() {
         return "redirect:dashboard/overview";
     }
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public String getUsersPage(){
+    public String getUsersPage(Model model) {
+        Collection<User> allUsers = userService.getAllUsers();
+        model.addAttribute("users", allUsers);
         return "admin/users";
     }
+
     @RequestMapping(value = "/overview", method = RequestMethod.GET)
-    public String getOverviewPage(){
+    public String getOverviewPage() {
         return "admin/overview";
     }
 
