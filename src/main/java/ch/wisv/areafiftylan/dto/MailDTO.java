4f3c7d6db819ab359f2a36b71d1fd53c8d7684f6
martijn9612 <martijn9612@gmail.com>
2016-01-03 18:32:34 +0100

    Use lombok for the DTO classes #95

--- src/main/java/ch/wisv/areafiftylan/dto/MailDTO.java
+++ src/main/java/ch/wisv/areafiftylan/dto/MailDTO.java
@@ -1,24 +1,13 @@
 package ch.wisv.areafiftylan.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class MailDTO {
 
+    @Getter @Setter
     String subject;
 
+    @Getter @Setter
     String message;
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
--- src/main/java/ch/wisv/areafiftylan/dto/ProfileDTO.java
+++ src/main/java/ch/wisv/areafiftylan/dto/ProfileDTO.java
@@ -1,98 +1,38 @@
 package ch.wisv.areafiftylan.dto;
 
 import ch.wisv.areafiftylan.model.util.Gender;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
 
 public class ProfileDTO {
 
-    @NotNull
+    @NotNull @Getter @Setter
     private Gender gender;
-    @NotEmpty
+
+    @NotEmpty @Getter @Setter
     private String address = "";
-    @NotEmpty
+
+    @NotEmpty @Getter @Setter
     private String zipcode = "";
-    @NotEmpty
+
+    @NotEmpty @Getter @Setter
     private String city = "";
-    @NotEmpty
+
+    @NotEmpty @Getter @Setter
     private String phoneNumber = "";
+
+    @Getter @Setter
     private String notes = "";
-    @NotEmpty
+
+    @NotEmpty @Getter @Setter
     private String firstName = "";
-    @NotEmpty
+
+    @NotEmpty @Getter @Setter
     private String lastName = "";
+
+    @Getter @Setter
     private String displayName = "";
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
 }
