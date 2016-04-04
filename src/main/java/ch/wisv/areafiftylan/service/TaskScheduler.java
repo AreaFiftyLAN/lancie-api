d973de5eb6e2a43fd1342176fcf93f63729565a9
Beer van der Drift <beerdrift@gmail.com>
2016-04-04 20:19:13 +0200

    Users now are deleted after expiring their verificationtoken.

--- src/main/java/ch/wisv/areafiftylan/service/TaskScheduler.java
+++ src/main/java/ch/wisv/areafiftylan/service/TaskScheduler.java
@@ -1,12 +1,18 @@
 package ch.wisv.areafiftylan.service;
 
 import ch.wisv.areafiftylan.model.Order;
+import ch.wisv.areafiftylan.model.User;
 import ch.wisv.areafiftylan.model.util.OrderStatus;
+import ch.wisv.areafiftylan.security.token.VerificationToken;
 import ch.wisv.areafiftylan.service.repository.OrderRepository;
+import ch.wisv.areafiftylan.service.repository.UserRepository;
+import ch.wisv.areafiftylan.service.repository.token.VerificationTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
@@ -23,13 +29,21 @@ public class TaskScheduler {
     private final int ORDER_STAY_ALIVE_MINUTES = 30;
     private final int ORDER_EXPIRY_CHECK_INTERVAL_SECONDS = 60;
 
+    private final int USER_CLEANUP_CHECK_INTERVAL_MINUTES = 60;
+
     private OrderRepository orderRepository;
     private OrderService orderService;
 
+    private UserRepository userRepository;
+    private VerificationTokenRepository verificationTokenRepository;
+
     @Autowired
-    public TaskScheduler(OrderRepository orderRepository, OrderService orderService) {
+    public TaskScheduler(OrderRepository orderRepository, OrderService orderService, VerificationTokenRepository verificationTokenRepository,
+                            UserRepository userRepository) {
         this.orderRepository = orderRepository;
         this.orderService = orderService;
+        this.userRepository = userRepository;
+        this.verificationTokenRepository = verificationTokenRepository;
     }
 
 
@@ -44,8 +58,20 @@ public class TaskScheduler {
         expiredOrders.forEach(o -> orderService.expireOrder(o));
     }
 
+    @Scheduled(fixedRate = USER_CLEANUP_CHECK_INTERVAL_MINUTES * 60 * 1000)
+    public void CleanUpUsers() {
+        LocalDateTime now = LocalDateTime.now();
+        List<VerificationToken> allExpiredVerificationTokens = verificationTokenRepository.findAllByExpiryDateBefore(now);
+        allExpiredVerificationTokens.forEach(t -> handleExpiredVerificationToken(t));
+    }
+
     public static Predicate<Order> isExpired() {
         return o -> o.getStatus().equals(OrderStatus.CREATING) || o.getStatus().equals(OrderStatus.EXPIRED) ||
                 o.getStatus().equals(OrderStatus.CANCELLED);
+    }
+
+    private void handleExpiredVerificationToken(VerificationToken verificationToken) {
+        verificationTokenRepository.delete(verificationToken);
+        userRepository.delete(verificationToken.getUser());
     }
 }
--- src/main/java/ch/wisv/areafiftylan/service/repository/token/VerificationTokenRepository.java
+++ src/main/java/ch/wisv/areafiftylan/service/repository/token/VerificationTokenRepository.java
@@ -4,8 +4,12 @@ import ch.wisv.areafiftylan.security.token.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VerificationTokenRepository extends TokenRepository<VerificationToken> {
+    List<VerificationToken> findAllByExpiryDateBefore(LocalDateTime date);
 }
