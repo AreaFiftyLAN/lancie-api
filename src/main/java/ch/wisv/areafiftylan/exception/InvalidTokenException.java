bb181e82f174084f48598c79d3e6cf52fbd0500b
Beer van der Drift <beerdrift@gmail.com>
2016-04-04 14:51:55 +0200

    Gave the InvalidTokenException a more proper HTTP response.

--- src/main/java/ch/wisv/areafiftylan/exception/InvalidTokenException.java
+++ src/main/java/ch/wisv/areafiftylan/exception/InvalidTokenException.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Created by beer on 11-3-16.
  */
 
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidTokenException extends RuntimeException{
     public InvalidTokenException() {
         super("Token is expired, has been already used or has been revoked.");
