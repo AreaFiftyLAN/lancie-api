daa34654a2abd7de44d7edec6b2be7770723724c
Sille <sille.kamoen@gmail.com>
2016-05-07 15:19:28 +0200

    Removed unused function from Token. Removed validToken check from SecurityConfig

--- src/main/java/ch/wisv/areafiftylan/security/TokenAuthenticationFilter.java
+++ src/main/java/ch/wisv/areafiftylan/security/TokenAuthenticationFilter.java
@@ -1,13 +1,11 @@
 package ch.wisv.areafiftylan.security;
 
-import ch.wisv.areafiftylan.exception.TokenNotFoundException;
 import ch.wisv.areafiftylan.model.User;
 import ch.wisv.areafiftylan.security.token.AuthenticationToken;
 import ch.wisv.areafiftylan.service.repository.token.AuthenticationTokenRepository;
 import com.google.common.base.Strings;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
-import org.springframework.security.web.authentication.www.NonceExpiredException;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -15,7 +13,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 class TokenAuthenticationFilter extends GenericFilterBean {
 
@@ -32,17 +32,26 @@ class TokenAuthenticationFilter extends GenericFilterBean {
         String xAuth = ((HttpServletRequest) request).getHeader("X-Auth-Token");
 
         if (!Strings.isNullOrEmpty(xAuth)) {
-            AuthenticationToken authenticationToken = authenticationTokenRepository.findByToken(xAuth)
-                    .orElseThrow(() -> new TokenNotFoundException(xAuth));
-
+            AuthenticationToken authenticationToken =
+                    extractOptional(authenticationTokenRepository.findByToken(xAuth), response);
             if (authenticationToken.isValid()) {
                 User user = authenticationToken.getUser();
                 SecurityContextHolder.getContext()
                         .setAuthentication(new PreAuthenticatedAuthenticationToken(user, "N/A", user.getAuthorities()));
             } else {
-                throw new NonceExpiredException("Token expired!");
+                ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token Expired");
             }
         }
         chain.doFilter(request, response);
+    }
+
+    private AuthenticationToken extractOptional(Optional<AuthenticationToken> op, ServletResponse response)
+            throws IOException {
+        if (!op.isPresent()) {
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token not found");
+            return null;
+        } else {
+            return op.get();
+        }
     }
 }
