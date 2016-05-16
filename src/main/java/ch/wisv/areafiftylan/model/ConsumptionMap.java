7c8aac536b26a8e94ca814d4888d0c3f6cf32caf
Beer van der Drift <beerdrift@gmail.com>
2016-05-16 13:12:08 +0200

    Added ticket dependency in the consumption map.

--- src/main/java/ch/wisv/areafiftylan/model/ConsumptionMap.java
+++ src/main/java/ch/wisv/areafiftylan/model/ConsumptionMap.java
@@ -1,16 +1,26 @@
 package ch.wisv.areafiftylan.model;
 
 import ch.wisv.areafiftylan.exception.ConsumptionNotSupportedException;
+import lombok.NonNull;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 import java.util.HashMap;
 
 /**
  * Created by beer on 8-5-16.
  */
+@Entity
 public class ConsumptionMap {
     public static HashMap<String, Boolean> MasterMap;
 
+    @NonNull
     private HashMap<String, Boolean> consumptionMap;
+
+    @OneToOne(targetEntity = Ticket.class, cascade = CascadeType.MERGE)
+    @NonNull
+    private Ticket ticket;
 
     public ConsumptionMap() {
         this.consumptionMap = MasterMap;
