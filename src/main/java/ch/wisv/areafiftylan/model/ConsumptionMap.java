383ac48c887a97e537117ef656efa98cd40ef80f
Beer van der Drift <beerdrift@gmail.com>
2016-05-16 13:57:43 +0200

    Altered ConsumptionMap to use a Collection instead of a HashMap.

--- src/main/java/ch/wisv/areafiftylan/model/ConsumptionMap.java
+++ src/main/java/ch/wisv/areafiftylan/model/ConsumptionMap.java
@@ -1,44 +1,64 @@
 package ch.wisv.areafiftylan.model;
 
+import ch.wisv.areafiftylan.exception.AlreadyConsumedException;
 import ch.wisv.areafiftylan.exception.ConsumptionNotSupportedException;
+import lombok.Getter;
 import lombok.NonNull;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by beer on 8-5-16.
  */
 @Entity
 public class ConsumptionMap {
-    public static HashMap<String, Boolean> MasterMap;
+    public static Collection<String> PossibleConsumptions;
 
     @NonNull
-    private HashMap<String, Boolean> consumptionMap;
+    private Collection<String> consumptionsMade;
 
     @OneToOne(targetEntity = Ticket.class, cascade = CascadeType.MERGE)
     @NonNull
+    @Getter
     private Ticket ticket;
 
     public ConsumptionMap() {
-        this.consumptionMap = MasterMap;
+        this.consumptionsMade = new ArrayList<>();
+    }
+
+    public boolean isConsumed(String consumption){
+        checkIfConsumptionAllowedAndThrowIfNot(consumption);
+
+        return consumptionsMade.contains(consumption);
     }
 
     public void consume(String consumption){
-        setConsumption(consumption, !MasterMap.get(consumption));
+        checkIfConsumptionAllowedAndThrowIfNot(consumption);
+
+        if(isConsumed(consumption)){
+            throw new AlreadyConsumedException(consumption);
+        }
+
+        consumptionsMade.add(consumption);
     }
 
     public void reset(String consumption){
-        setConsumption(consumption, MasterMap.get(consumption));
+        checkIfConsumptionAllowedAndThrowIfNot(consumption);
+
+        if(isConsumed(consumption)){
+            consumptionsMade.remove(consumption);
+        }
     }
 
-    private void setConsumption(String consumption, boolean value){
-        if(!MasterMap.containsKey(consumption)){
+    private void checkIfConsumptionAllowedAndThrowIfNot(String consumption){
+        if(!PossibleConsumptions.contains(consumption)) {
             throw new ConsumptionNotSupportedException(consumption);
         }
-
-        consumptionMap.put(consumption, value);
     }
 }
