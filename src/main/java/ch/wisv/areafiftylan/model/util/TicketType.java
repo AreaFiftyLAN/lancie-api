face2c26dac832238cc220408fa65f097d56b5f0
Sille <sille.kamoen@gmail.com>
2016-05-18 14:26:13 +0200

    Add deadline to TicketType and introdced LAST_MINUTE ticket

--- src/main/java/ch/wisv/areafiftylan/model/util/TicketType.java
+++ src/main/java/ch/wisv/areafiftylan/model/util/TicketType.java
@@ -1,16 +1,22 @@
 package ch.wisv.areafiftylan.model.util;
 
+import java.time.LocalDateTime;
+
 public enum TicketType {
-    EARLY_FULL("Early Bird", 37.50F, 50), REGULAR_FULL("Regular", 40.00F, 150);
+    EARLY_FULL("Early Bird", 37.50F, 50, LocalDateTime.of(2016, 6, 3, 0, 0)),
+    REGULAR_FULL("Regular", 40.00F, 0, LocalDateTime.of(2016, 5, 29, 0, 0)),
+    LAST_MINUTE("Last Minute", 45.00F, 0, LocalDateTime.of(2016, 5, 29, 0, 0));
 
     private final float price;
     private final int limit;
     private final String text;
+    private final LocalDateTime deadline;
 
-    TicketType(String text, float price, int limit) {
+    TicketType(String text, float price, int limit, LocalDateTime deadline) {
         this.text = text;
         this.price = price;
         this.limit = limit;
+        this.deadline = deadline;
     }
 
     public float getPrice() {
@@ -23,5 +29,9 @@ public enum TicketType {
 
     public String getText() {
         return text;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
     }
 }
