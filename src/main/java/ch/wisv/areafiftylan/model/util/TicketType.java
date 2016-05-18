57cd68eada2d72a304e1fd3d969b891e1f134178
Sille <sille.kamoen@gmail.com>
2016-05-18 16:37:27 +0200

    Change deadline for last minute ticket

--- src/main/java/ch/wisv/areafiftylan/model/util/TicketType.java
+++ src/main/java/ch/wisv/areafiftylan/model/util/TicketType.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public enum TicketType {
     EARLY_FULL("Early Bird", 37.50F, 50, LocalDateTime.of(2016, 6, 3, 0, 0)),
     REGULAR_FULL("Regular", 40.00F, 0, LocalDateTime.of(2016, 5, 29, 0, 0)),
-    LAST_MINUTE("Last Minute", 45.00F, 0, LocalDateTime.of(2016, 5, 29, 0, 0));
+    LAST_MINUTE("Last Minute", 45.00F, 0, LocalDateTime.of(2016, 6, 3, 0, 0));
 
     private final float price;
     private final int limit;
