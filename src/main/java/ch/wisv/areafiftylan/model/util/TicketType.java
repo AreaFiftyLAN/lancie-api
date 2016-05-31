05f2498e84c3a30a49087d03fc8b1b205f5af420
Martijn Janssen <martijn9612@users.noreply.github.com>
2016-05-31 19:48:42 +0200

    Lower price for late bird tickets (#244)

--- src/main/java/ch/wisv/areafiftylan/model/util/TicketType.java
+++ src/main/java/ch/wisv/areafiftylan/model/util/TicketType.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 public enum TicketType {
     EARLY_FULL("Early Bird", 37.50F, 50, LocalDateTime.of(2016, 6, 3, 0, 0)),
     REGULAR_FULL("Regular", 40.00F, 0, LocalDateTime.of(2016, 5, 28, 23, 59)),
-    LAST_MINUTE("Last Minute", 45.00F, 0, LocalDateTime.of(2016, 6, 2, 23, 59));
+    LAST_MINUTE("Last Minute", 42.50F, 0, LocalDateTime.of(2016, 6, 2, 23, 59));
 
     private final float price;
     private final int limit;
