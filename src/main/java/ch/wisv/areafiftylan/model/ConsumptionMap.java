e531ec4a23da752aaf6fde7e7b5742a3f5f099a6
Martijn Janssen <martijn9612@users.noreply.github.com>
2016-06-03 16:11:50 +0200

    Fix consumption relation (#255)
    * Fix relation
    
    * Fixed consumptions made collection fetchtype.
    
    * Added test of consumption of two tickets in succession.

--- src/main/java/ch/wisv/areafiftylan/model/ConsumptionMap.java
+++ src/main/java/ch/wisv/areafiftylan/model/ConsumptionMap.java
@@ -21,7 +21,7 @@ public class ConsumptionMap {
     Long id;
 
     @NonNull
-    @ElementCollection(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.EAGER)
     private Collection<Consumption> consumptionsMade;
 
     @OneToOne(targetEntity = Ticket.class, cascade = CascadeType.MERGE)
