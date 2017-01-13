d111f8f4806b8d4874774e514adf1c48d0551608
Matthijs Kok <matthijs.kok@me.com>
2017-01-13 18:52:57 +0100

    Add sponsors.

---
+++ src/main/java/ch/wisv/areafiftylan/web/sponsor/service/SponsorRepository.java
@@ -0,0 +1,15 @@
+package ch.wisv.areafiftylan.web.sponsor.service;
+
+import ch.wisv.areafiftylan.web.sponsor.model.Sponsor;
+import ch.wisv.areafiftylan.web.sponsor.model.SponsorType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+
+@Repository
+public interface SponsorRepository extends JpaRepository<Sponsor, Long> {
+
+    Collection<Sponsor> findByType(SponsorType type);
+
+}
