8065142d10abf8bb33c4dfbad591c7f4a6af2078
Sille Kamoen <sille.kamoen@gmail.com>
2017-02-22 15:00:48 +0100

    Replace CORSfilter as OncePerRequest, and add OPTIONS support

--- src/main/java/ch/wisv/areafiftylan/security/CORSFilter.java
+++ src/main/java/ch/wisv/areafiftylan/security/CORSFilter.java
@@ -18,34 +18,30 @@
 package ch.wisv.areafiftylan.security;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class CORSFilter implements Filter {
+public class CORSFilter extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-            throws IOException, ServletException {
-
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-        response.setHeader("Access-Control-Expose-Headers", "X-Auth-Token");
-
-        chain.doFilter(req, res);
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) {
+        response.setHeader("Access-Control-Allow-Headers", "X-Auth-Token, Accept, Content-Type, Origin");
+        response.addHeader("Access-Control-Expose-Headers", "X-Auth-Token");
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
     }
 
     @Override
