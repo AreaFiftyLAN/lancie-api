d25bd93c3e2a10a4d60897d41d3803b7abe6c4ed
Matthijs Kok <matthijs.kok@me.com>
2017-03-18 11:55:27 +0100

    Add annotation. Cast MailTemplate directly. Add comment. Add first mail template file.

--- src/main/java/ch/wisv/areafiftylan/utils/mail/MailTemplateServiceJSONImpl.java
+++ src/main/java/ch/wisv/areafiftylan/utils/mail/MailTemplateServiceJSONImpl.java
@@ -1,25 +1,30 @@
 package ch.wisv.areafiftylan.utils.mail;
 
 import ch.wisv.areafiftylan.exception.MailTemplateNotFoundException;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Service;
 
 import java.io.FileReader;
 import java.io.IOException;
 
+@Service
 public class MailTemplateServiceJSONImpl implements MailTemplateService {
 
+    /**
+     * Reads a JSON file and casts it to a MailTemplate.
+     * This method assumes correctness of the JSON file.
+     *
+     * @param templateName The name of the JSON file.
+     * @return MailTemplate if successful, null otherwise.
+     */
     @Override
     public MailTemplate getMailTemplateByName(String templateName) {
         MailTemplate mailTemplate;
         JSONParser jsonParser = new JSONParser();
         try {
             Object object = jsonParser.parse(new FileReader("config/mail/" + templateName + ".json"));
-            JSONObject jsonObject = (JSONObject) object;
-            mailTemplate = new MailTemplate();
-            mailTemplate.setSubject((String) jsonObject.get("subject"));
-            mailTemplate.setMessage((String) jsonObject.get("message"));
+            mailTemplate = (MailTemplate) object;
         } catch (IOException | ParseException e) {
             throw new MailTemplateNotFoundException(templateName);
         }
