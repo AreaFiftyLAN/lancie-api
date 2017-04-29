19aa348df0943edc97aacd76ad9ff098200fa2ef
Matthijs Kok <matthijs.kok@me.com>
2017-04-29 21:13:27 +0200

    Enum magic ofzo

--- src/main/java/ch/wisv/areafiftylan/exception/AreaFiftyLANException.java
+++ src/main/java/ch/wisv/areafiftylan/exception/AreaFiftyLANException.java
@@ -2,31 +2,14 @@ package ch.wisv.areafiftylan.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+
 @Slf4j
 public class AreaFiftyLANException extends RuntimeException {
 
     public AreaFiftyLANException(AreaFiftyLANExceptionLogLevelEnum logLevelEnum, String message) {
         super(message);
-        switch (logLevelEnum) {
-            case ERROR:
-                log.error(message);
-                break;
-            case WARN:
-                log.warn(message);
-                break;
-            case INFO:
-                log.info(message);
-                break;
-            case DEBUG:
-                log.debug(message);
-                break;
-            case TRACE:
-                log.trace(message);
-                break;
-            default:
-                log.error(message);
-                break;
-        }
+        logLevelEnum.getConsumer().accept(message);
     }
 
     public AreaFiftyLANException(String message) {
--- src/main/java/ch/wisv/areafiftylan/exception/AreaFiftyLANExceptionLogLevelEnum.java
+++ src/main/java/ch/wisv/areafiftylan/exception/AreaFiftyLANExceptionLogLevelEnum.java
@@ -1,9 +1,23 @@
 package ch.wisv.areafiftylan.exception;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Consumer;
+@Slf4j
 enum AreaFiftyLANExceptionLogLevelEnum {
-    ERROR,
-    WARN,
-    INFO,
-    DEBUG,
-    TRACE
+    ERROR(log::error),
+    WARN(log::warn),
+    INFO(log::info),
+    DEBUG(log::debug),
+    TRACE(log::trace);
+
+    private Consumer consumer;
+
+    AreaFiftyLANExceptionLogLevelEnum(Consumer<String> consumer) {
+        this.consumer = consumer;
+    }
+
+    public Consumer<String> getConsumer() {
+        return consumer;
+    }
 }
