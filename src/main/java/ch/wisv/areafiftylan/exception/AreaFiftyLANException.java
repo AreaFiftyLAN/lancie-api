dc11becde84c8029f84d912be8530c71caf18fff
Tim van der Lippe <tvanderlippe@gmail.com>
2017-04-30 14:32:25 +0200

    Use pure lambdas instead

--- src/main/java/ch/wisv/areafiftylan/exception/AreaFiftyLANException.java
+++ src/main/java/ch/wisv/areafiftylan/exception/AreaFiftyLANException.java
@@ -2,12 +2,33 @@ package ch.wisv.areafiftylan.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.function.Consumer;
+
 @Slf4j
 public class AreaFiftyLANException extends RuntimeException {
 
+    public enum AreaFiftyLANExceptionLogLevelEnum {
+        ERROR(log::error),
+        WARN(log::warn),
+        INFO(log::info),
+        DEBUG(log::debug),
+        TRACE(log::trace);
+
+        private final Consumer<String> logger;
+
+        AreaFiftyLANExceptionLogLevelEnum(Consumer<String> logger) {
+            this.logger = logger;
+        }
+
+        public void logMessage(String message) {
+            logger.accept(message);
+        }
+    }
+
+
     public AreaFiftyLANException(AreaFiftyLANExceptionLogLevelEnum logEnum, String message) {
         super(message);
-        logEnum.apply(message);
+        logEnum.logMessage(message);
     }
 
     public AreaFiftyLANException(String message) {
