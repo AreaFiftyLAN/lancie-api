6a43666d2bc7007eabd1eeeda0eb621db4111e2b
Matthijs Kok <matthijs.kok@me.com>
2017-05-04 13:26:14 +0200

    Remove method override. Shorten enum name.

--- src/main/java/ch/wisv/areafiftylan/exception/AreaFiftyLANException.java
+++ src/main/java/ch/wisv/areafiftylan/exception/AreaFiftyLANException.java
@@ -7,7 +7,7 @@ import java.util.function.Consumer;
 @Slf4j
 public class AreaFiftyLANException extends RuntimeException {
 
-    public enum AreaFiftyLANExceptionLogLevelEnum {
+    public enum LogLevelEnum {
         ERROR(log::error),
         WARN(log::warn),
         INFO(log::info),
@@ -16,7 +16,7 @@ public class AreaFiftyLANException extends RuntimeException {
 
         private final Consumer<String> logger;
 
-        AreaFiftyLANExceptionLogLevelEnum(Consumer<String> logger) {
+        LogLevelEnum(Consumer<String> logger) {
             this.logger = logger;
         }
 
@@ -25,18 +25,12 @@ public class AreaFiftyLANException extends RuntimeException {
         }
     }
 
-
-    public AreaFiftyLANException(AreaFiftyLANExceptionLogLevelEnum logEnum, String message) {
+    public AreaFiftyLANException(LogLevelEnum logEnum, String message) {
         super(message);
         logEnum.logMessage(message);
     }
 
     public AreaFiftyLANException(String message) {
-        this(AreaFiftyLANExceptionLogLevelEnum.WARN, message);
-    }
-
-    @Override
-    public synchronized Throwable fillInStackTrace() {
-        return this;
+        this(LogLevelEnum.WARN, message);
     }
 }
