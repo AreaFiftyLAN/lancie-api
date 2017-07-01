ab0733aea16e21065bb20e7aa08867101b337591
Beer van der Drift <beerdrift@gmail.com>
2017-07-01 13:54:47 +0200

    Created TeamJoinToken

---
+++ src/main/java/ch/wisv/areafiftylan/security/token/TeamJoinToken.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016  W.I.S.V. 'Christiaan Huygens'
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ch.wisv.areafiftylan.security.token;
+
+import ch.wisv.areafiftylan.teams.model.Team;
+import ch.wisv.areafiftylan.users.model.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+@NoArgsConstructor
+public class TeamJoinToken extends Token {
+
+    @Getter
+    @OneToOne(targetEntity = Team.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false)
+    private Team team;
+
+    public TeamJoinToken(Team team) {
+        super(0);
+        this.team = team;
+    }
+}
