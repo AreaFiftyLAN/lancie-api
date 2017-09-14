11c48f94b5ac73ee127f0b7dd7d4f2333338ef4e
Matthijs Kok <matthijs.kok2@gmail.com>
2017-09-14 17:29:03 +0200

    Add FAQ CRUD (#437)
    * Add FAQ CRUD
    
    * Suggested changes
    
    * Fix tests

---
+++ src/main/java/ch/wisv/areafiftylan/web/faq/controller/FaqController.java
@@ -0,0 +1,51 @@
+package ch.wisv.areafiftylan.web.faq.controller;
+
+import ch.wisv.areafiftylan.web.faq.model.FaqPair;
+import ch.wisv.areafiftylan.web.faq.service.FaqService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+import static ch.wisv.areafiftylan.utils.ResponseEntityBuilder.createResponseEntity;
+
+@Controller
+@RequestMapping("/web/faq")
+public class FaqController {
+
+    private FaqService faqService;
+
+    public FaqController(FaqService faqService) {
+        this.faqService = faqService;
+    }
+
+    @GetMapping
+    ResponseEntity<?> getFaq() {
+        Collection<FaqPair> faq = faqService.getFaq();
+        return createResponseEntity(HttpStatus.OK, "FAQ retrieved.", faq);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('COMMITTEE')")
+    ResponseEntity<?> addQuestion(@RequestBody FaqPair faqPair) {
+        faqPair = faqService.addQuestion(faqPair);
+        return createResponseEntity(HttpStatus.CREATED, "FaqPair added.", faqPair);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('COMMITTEE')")
+    ResponseEntity<?> deleteQuestion(@PathVariable Long id) {
+        faqService.deleteQuestion(id);
+        return createResponseEntity(HttpStatus.OK, "FaqPair deleted.");
+    }
+
+    @DeleteMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    ResponseEntity<?> deleteFaq() {
+        faqService.deleteFaq();
+        return createResponseEntity(HttpStatus.OK, "Faq deleted.");
+    }
+}
---
+++ src/test/java/ch/wisv/areafiftylan/integration/WebFaqIntegrationTest.java
@@ -0,0 +1,190 @@
+package ch.wisv.areafiftylan.integration;
+
+import ch.wisv.areafiftylan.users.model.User;
+import ch.wisv.areafiftylan.web.faq.model.FaqPair;
+import ch.wisv.areafiftylan.web.faq.service.FaqRepository;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+
+public class WebFaqIntegrationTest  extends XAuthIntegrationTest{
+
+    @Autowired
+    private FaqRepository faqRepository;
+
+    private final String FAQ_ENDPOINT = "/web/faq/";
+
+    private FaqPair createPair() {
+        FaqPair faqPair = new FaqPair();
+        faqPair.setQuestion("What do you get if you multiply six by nine?");
+        faqPair.setAnswer("42");
+        return faqRepository.save(faqPair);
+    }
+
+    @After
+    public void cleanupFaqTests() {
+        faqRepository.deleteAll();
+    }
+
+    @Test
+    public void testGetFaqAsUser() {
+        User user = createUser();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(user)).
+        when().
+            get(FAQ_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_OK);
+        //@formatter:on
+    }
+
+    @Test
+    public void testGetFaqAsCommitteeMember() {
+        User committeeMember = createCommitteeMember();
+        FaqPair faqPair = createPair();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(committeeMember)).
+        when().
+            get(FAQ_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_OK).
+            body("message", is("FAQ retrieved.")).
+            body("object.question", hasItem(faqPair.getQuestion()));
+        //@formatter:on
+    }
+
+    @Test
+    public void testAddQuestionAsUser() {
+        User user = createUser();
+        FaqPair faqPair = createPair();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(user)).
+            body(faqPair).
+            contentType(ContentType.JSON).
+        when().
+            post(FAQ_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_FORBIDDEN);
+        //@formatter:on
+    }
+
+    @Test
+    public void testAddQuestionAsCommitteeMember() {
+        User committeeMember = createCommitteeMember();
+        FaqPair faqPair = createPair();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(committeeMember)).
+            body(faqPair).
+            contentType(ContentType.JSON).
+        when().
+            post(FAQ_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_CREATED).
+            body("message", is("FaqPair added.")).
+            body("object.question", is(faqPair.getQuestion()));
+        //@formatter:on
+    }
+
+    @Test
+    public void testDeleteQuestionAsUser() {
+        User user = createUser();
+        FaqPair faqPair = createPair();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(user)).
+            body(faqPair).
+            contentType(ContentType.JSON).
+        when().
+            delete(FAQ_ENDPOINT + faqPair.getId()).
+        then().
+            statusCode(HttpStatus.SC_FORBIDDEN);
+        //@formatter:on
+    }
+
+    @Test
+    public void testDeleteQuestionAsCommitteeMember() {
+        User committeeMember = createCommitteeMember();
+        FaqPair faqPair = createPair();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(committeeMember)).
+            body(faqPair).
+            contentType(ContentType.JSON).
+        when().
+            delete(FAQ_ENDPOINT + faqPair.getId()).
+        then().
+            statusCode(HttpStatus.SC_OK).
+            body("message", is("FaqPair deleted."));
+        //@formatter:on
+    }
+
+    @Test
+    public void testDeleteFaqAsUser() {
+        User user = createUser();
+        FaqPair faqPair = createPair();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(user)).
+            body(faqPair).
+            contentType(ContentType.JSON).
+        when().
+            delete(FAQ_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_FORBIDDEN);
+        //@formatter:on
+    }
+
+    @Test
+    public void testDeleteFaqAsCommitteeMember() {
+        User committeeMember = createCommitteeMember();
+        FaqPair faqPair = createPair();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(committeeMember)).
+            body(faqPair).
+            contentType(ContentType.JSON).
+        when().
+            delete(FAQ_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_FORBIDDEN);
+        //@formatter:on
+    }
+
+    @Test
+    public void testDeleteFaqAsAdmin() {
+        User admin = createAdmin();
+        FaqPair faqPair = createPair();
+
+        //@formatter:off
+        given().
+            header(getXAuthTokenHeaderForUser(admin)).
+            body(faqPair).
+            contentType(ContentType.JSON).
+        when().
+            delete(FAQ_ENDPOINT).
+        then().
+            statusCode(HttpStatus.SC_OK).
+            body("message", is("Faq deleted."));
+        //@formatter:on
+    }
+
+
+}
