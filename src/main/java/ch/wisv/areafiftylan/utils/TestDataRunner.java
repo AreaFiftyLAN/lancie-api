450cb51dfc547bdf59423818ec1185c03f79372e
Matthijs Kok <matthijs.kok2@gmail.com>
2017-09-15 21:24:22 +0200

    Expand TestDataRunner (#441)
    * Add Web data to TestDataRunner.
    
    * Further expand and clarify TestDataRunner.
    
    * Ignore TestDataRunner in CodeCov
    
    * Ignore database migration files too while we're at it.
    
    * Change names of committee members

--- src/main/java/ch/wisv/areafiftylan/utils/TestDataRunner.java
+++ src/main/java/ch/wisv/areafiftylan/utils/TestDataRunner.java
@@ -18,7 +18,6 @@
 package ch.wisv.areafiftylan.utils;
 
 import ch.wisv.areafiftylan.extras.consumption.model.Consumption;
-import ch.wisv.areafiftylan.extras.consumption.model.ConsumptionMapsRepository;
 import ch.wisv.areafiftylan.extras.consumption.model.PossibleConsumptionsRepository;
 import ch.wisv.areafiftylan.extras.consumption.service.ConsumptionService;
 import ch.wisv.areafiftylan.extras.rfid.model.RFIDLink;
@@ -37,6 +36,16 @@ import ch.wisv.areafiftylan.users.model.Gender;
 import ch.wisv.areafiftylan.users.model.Role;
 import ch.wisv.areafiftylan.users.model.User;
 import ch.wisv.areafiftylan.users.service.UserRepository;
+import ch.wisv.areafiftylan.web.committee.model.CommitteeMember;
+import ch.wisv.areafiftylan.web.committee.service.CommitteeRepository;
+import ch.wisv.areafiftylan.web.faq.model.FaqPair;
+import ch.wisv.areafiftylan.web.faq.service.FaqRepository;
+import ch.wisv.areafiftylan.web.sponsor.model.Sponsor;
+import ch.wisv.areafiftylan.web.sponsor.model.SponsorType;
+import ch.wisv.areafiftylan.web.sponsor.service.SponsorRepository;
+import ch.wisv.areafiftylan.web.tournament.model.Tournament;
+import ch.wisv.areafiftylan.web.tournament.model.TournamentType;
+import ch.wisv.areafiftylan.web.tournament.service.TournamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -45,6 +54,9 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 @Profile("dev")
@@ -57,15 +69,21 @@ public class TestDataRunner implements CommandLineRunner {
     private final TicketTypeRepository ticketTypeRepository;
     private final RFIDLinkRepository rfidLinkRepository;
     private final PossibleConsumptionsRepository consumptionsRepository;
-    private final ConsumptionMapsRepository consumptionMapsRepository;
     private final ConsumptionService consumptionService;
+
+    private final CommitteeRepository committeeRepository;
+    private final FaqRepository faqRepository;
+    private final SponsorRepository sponsorRepository;
+    private final TournamentRepository tournamentRepository;
 
     @Autowired
     public TestDataRunner(UserRepository accountRepository, TicketRepository ticketRepository,
                           TeamRepository teamRepository, SeatService seatService,
                           TicketOptionRepository ticketOptionRepository, TicketTypeRepository ticketTypeRepository,
                           RFIDLinkRepository rfidLinkRepository, PossibleConsumptionsRepository consumptionsRepository,
-                          ConsumptionMapsRepository consumptionMapsRepository, ConsumptionService consumptionService) {
+                          ConsumptionService consumptionService, CommitteeRepository committeeRepository,
+                          FaqRepository faqRepository, SponsorRepository sponsorRepository,
+                          TournamentRepository tournamentRepository) {
         this.accountRepository = accountRepository;
         this.ticketRepository = ticketRepository;
         this.seatService = seatService;
@@ -74,83 +92,117 @@ public class TestDataRunner implements CommandLineRunner {
         this.ticketTypeRepository = ticketTypeRepository;
         this.rfidLinkRepository = rfidLinkRepository;
         this.consumptionsRepository = consumptionsRepository;
-        this.consumptionMapsRepository = consumptionMapsRepository;
         this.consumptionService = consumptionService;
+        this.committeeRepository = committeeRepository;
+        this.faqRepository = faqRepository;
+        this.sponsorRepository = sponsorRepository;
+        this.tournamentRepository = tournamentRepository;
     }
 
     @Override
     public void run(String... evt) throws Exception {
+        //region Users
         LocalDate localDate = LocalDate.of(2000, 1, 2);
 
-        User testUser1 = new User("user@mail.com", new BCryptPasswordEncoder().encode("password"));
-        testUser1.addRole(Role.ROLE_ADMIN);
-        testUser1.getProfile()
+        User userAdmin = new User("admin@mail.com", new BCryptPasswordEncoder().encode("password"));
+        userAdmin.addRole(Role.ROLE_ADMIN);
+        User userNormal = new User("user@mail.com", new BCryptPasswordEncoder().encode("password"));
+        User userCaptain = new User("captain@mail.com", new BCryptPasswordEncoder().encode("password"));
+        User userInvalidTicket = new User("invalidticket@mail.com", new BCryptPasswordEncoder().encode("password"));
+        User userNoTicket = new User("noticket@mail.com", new BCryptPasswordEncoder().encode("password"));
+        userAdmin.getProfile()
                 .setAllFields("Jan", "de Groot", "MonsterKiller9001", LocalDate.of(1990, 2, 1), Gender.MALE, "Mekelweg 4", "2826CD",
                         "Delft", "0906-0666", null);
-        User testUser2 = new User("bert@mail.com", new BCryptPasswordEncoder().encode("password"));
-        testUser2.getProfile()
+        userNormal.getProfile()
                 .setAllFields("Bert", "Kleijn", "ILoveZombies", localDate, Gender.OTHER, "Mekelweg 20", "2826CD",
                         "Amsterdam", "0611", null);
-        User testUser3 = new User("katrien@ms.com", new BCryptPasswordEncoder().encode("password"));
-        testUser3.getProfile()
+        userInvalidTicket.getProfile()
                 .setAllFields("Katrien", "Zwanenburg", "Admiral Cheesecake", localDate, Gender.FEMALE, "Ganzenlaan 5",
                         "2826CD", "Duckstad", "0906-0666", null);
-        User testUser4 = new User("user@yahoo.com", new BCryptPasswordEncoder().encode("password"));
-        testUser4.getProfile()
+        userNoTicket.getProfile()
                 .setAllFields("Kees", "Jager", "l33tz0r", localDate, Gender.MALE, "Herenweg 2", "2826CD", "Delft",
                         "0902-30283", null);
-        User testUser5 = new User("custom@myself.com", new BCryptPasswordEncoder().encode("password"));
-        testUser5.getProfile()
+        userCaptain.getProfile()
                 .setAllFields("Gert", "Gertson", "Whosyourdaddy", localDate, Gender.MALE, "Jansstraat", "8826CD",
                         "Delft", "0238-2309736", null);
 
-        testUser1 = accountRepository.saveAndFlush(testUser1);
-        testUser2 = accountRepository.saveAndFlush(testUser2);
-        testUser3 = accountRepository.saveAndFlush(testUser3);
-        testUser4 = accountRepository.saveAndFlush(testUser4);
-        testUser5 = accountRepository.saveAndFlush(testUser5);
-
+        userAdmin = accountRepository.saveAndFlush(userAdmin);
+        userNormal = accountRepository.saveAndFlush(userNormal);
+        userCaptain = accountRepository.saveAndFlush(userCaptain);
+        userInvalidTicket = accountRepository.saveAndFlush(userInvalidTicket);
+        userNoTicket = accountRepository.saveAndFlush(userNoTicket);
+        //endregion Users
+        //region Tickets
         TicketOption chMember = ticketOptionRepository.save(new TicketOption("chMember", -5F));
         TicketOption pickupService = ticketOptionRepository.save(new TicketOption("pickupService", 2.5F));
+
         TicketType early = new TicketType("Early", "Early Bird", 35F, 50, LocalDateTime.now().plusDays(7L), true);
         early.addPossibleOption(chMember);
         early.addPossibleOption(pickupService);
         early = ticketTypeRepository.save(early);
 
-        Ticket ticket = new Ticket(testUser1, early);
-        ticket.addOption(chMember);
-        ticket.addOption(pickupService);
-        Ticket ticket2 = new Ticket(testUser2, early);
-        ticket2.addOption(pickupService);
-        Ticket ticket3 = new Ticket(testUser3, early);
-        ticket.setValid(true);
-        ticket2.setValid(true);
-        ticketRepository.save(ticket);
-        ticketRepository.save(ticket2);
-        ticketRepository.save(ticket3);
+        TicketType normal = new TicketType("Normal", "Normal Ticket", 40F, 0, LocalDateTime.now().plusDays(7L), true);
+        normal.addPossibleOption(chMember);
+        normal.addPossibleOption(pickupService);
+        normal = ticketTypeRepository.save(normal);
 
-        RFIDLink rfidLink1 = new RFIDLink("0000000001", ticket);
+        TicketType late = new TicketType("Late", "Last Minute", 42.5F, 0, LocalDateTime.now().plusDays(14L), true);
+        late.addPossibleOption(chMember);
+        late.addPossibleOption(pickupService);
+        late = ticketTypeRepository.save(late);
+
+
+        Ticket ticketAdmin = new Ticket(userAdmin, normal);
+        ticketAdmin.addOption(chMember);
+        ticketAdmin.addOption(pickupService);
+        ticketAdmin.setValid(true);
+        ticketRepository.save(ticketAdmin);
+
+        Ticket ticketCaptain = new Ticket(userCaptain, early);
+        ticketCaptain.addOption(chMember);
+        ticketCaptain.addOption(pickupService);
+        ticketCaptain.setValid(true);
+        ticketRepository.save(ticketCaptain);
+
+        Ticket ticketNormal = new Ticket(userNormal, normal);
+        ticketNormal.addOption(pickupService);
+        ticketNormal.setValid(true);
+        ticketRepository.save(ticketNormal);
+
+        Ticket ticketInvalid = new Ticket(userInvalidTicket, late);
+        ticketInvalid.setValid(false);
+        ticketRepository.save(ticketInvalid);
+        //endregion Tickets
+        //region RFID
+        RFIDLink rfidLink1 = new RFIDLink("0000000001", ticketAdmin);
+        RFIDLink rfidLink2 = new RFIDLink("0000000002", ticketCaptain);
+        RFIDLink rfidLink3 = new RFIDLink("0000000003", ticketNormal);
         rfidLinkRepository.saveAndFlush(rfidLink1);
-        RFIDLink rfidLink2 = new RFIDLink("0000000002", ticket2);
         rfidLinkRepository.saveAndFlush(rfidLink2);
-        RFIDLink rfidLink3 = new RFIDLink("0000000003", ticket3);
         rfidLinkRepository.saveAndFlush(rfidLink3);
+        //endregion RFID
+        //region Consumptions
+        Consumption ontbijt = new Consumption("Ontbijt");
+        ontbijt = consumptionsRepository.saveAndFlush(ontbijt);
+        Consumption lunch = new Consumption("Lunch");
+        lunch = consumptionsRepository.saveAndFlush(lunch);
 
-        Consumption consumption1 = new Consumption("Bier");
-        consumption1 = consumptionsRepository.saveAndFlush(consumption1);
-        Consumption consumption2 = new Consumption("Wijn");
-        consumption2 = consumptionsRepository.saveAndFlush(consumption2);
-
-        consumptionService.consume(ticket.getId(), consumption1.getId());
-        consumptionService.consume(ticket.getId(), consumption2.getId());
-        consumptionService.consume(ticket2.getId(), consumption1.getId());
-
-        Team team = new Team("testTeam", testUser1);
-        team.addMember(testUser2);
-        team.addMember(testUser3);
-
+        consumptionService.consume(ticketAdmin.getId(), ontbijt.getId());
+        consumptionService.consume(ticketAdmin.getId(), lunch.getId());
+        consumptionService.consume(ticketCaptain.getId(), ontbijt.getId());
+        consumptionService.consume(ticketNormal.getId(), ontbijt.getId());
+        //endregion Consumptions
+        //region Team
+        Team team = new Team("Test Team", userCaptain);
+        team.addMember(userNormal);
+        team.addMember(userInvalidTicket);
         teamRepository.save(team);
-
+        Team team2 = new Team("Admin's Team", userAdmin);
+        team2.addMember(userCaptain);
+        team2.addMember(userNoTicket);
+        teamRepository.save(team2);
+        //endregion Team
+        //region Seat
         for (char s = 'A'; s <= 'J'; s++) {
             SeatGroupDTO seatGroup = new SeatGroupDTO();
             seatGroup.setNumberOfSeats(16);
@@ -158,6 +210,83 @@ public class TestDataRunner implements CommandLineRunner {
             seatService.addSeats(seatGroup);
         }
         seatService.setAllSeatsLock(false);
-        seatService.reserveSeat("A", 2, ticket.getId(), false);
+        seatService.reserveSeat("A", 1, ticketAdmin.getId(), false);
+        seatService.reserveSeat("A", 2, ticketCaptain.getId(), false);
+        seatService.reserveSeat("B", 1, ticketNormal.getId(), false);
+        //endregion Seat
+        //region Web Data
+        committeeMember(1L, "Chairman", "Mark Rutte", "group");
+        committeeMember(2L, "Secretary", "Lodewijk Asscher", "male");
+        committeeMember(3L, "Treasurer", "Jeroen Dijsselbloem", "money");
+        committeeMember(4L, "Commissioner of Promo", "Frans Timmermans", "bullhorn");
+        committeeMember(5L, "Commissioner of Logistics", "Melanie Schultz", "truck");
+        committeeMember(6L, "Commissioner of Systems", "Klaas Dijkhoff", "cogs");
+        committeeMember(7L, "Qualitate Qua", "Ivo Opstelten", "heart");
+
+        faqpair("What the fox say?", "Ring-ding-ding-ding-dingeringeding!");
+        faqpair("What do you get if you multiply six by nine?", "42");
+        faqpair("What is your favorite colour?", "Blue.");
+        faqpair("What is the capital of Assyria?", "Well I don't know!");
+        faqpair("What is your favorite colour?", "Blue! no, Yellow!");
+        faqpair("What is the airspeed velocity of an unladen swallow?", "What do you mean? African or European swallow?");
+
+        sponsor("Christiaan Huygens", SponsorType.PRESENTER, "https://ch.tudelft.nl", "images-optimized/lancie/logo_CH.png");
+        sponsor("TU Delft", SponsorType.PRESENTER, "https://www.tudelft.nl", "images-optimized/logos/logo_SC.png");
+        Sponsor sogeti = sponsor("Sogeti", SponsorType.PREMIUM, "https://www.sogeti.nl/", "images-optimized/logos/sogeti.png");
+        sponsor("Nutanix", SponsorType.PREMIUM, "https://www.nutanix.com", "images-optimized/logos/nutanix.png");
+        sponsor("OGD", SponsorType.PREMIUM, "https://ogd.nl/", "images-optimized/logos/ogd.png");
+        Sponsor coolerMaster = sponsor("CoolerMaster", SponsorType.NORMAL, "http://www.coolermaster.com/", "images-optimized/logos/Cooler_Master_Logo.png");
+        sponsor("Spam", SponsorType.NORMAL, "http://www.spam-energydrink.com/", "images-optimized/logos/spam-logo.jpg");
+        sponsor("Jigsaw", SponsorType.NORMAL, "http://www.jigsaw.nl/", "images-optimized/logos/jigsaw_cleaner_logo.png");
+        sponsor("TransIP", SponsorType.NORMAL, "https://www.transip.nl/", "images-optimized/logos/transip_logo.png");
+
+        tournament(TournamentType.OFFICIAL, "RL", "images-optimized/activities/rl.jpg", "3 V 3", "Rocket League",
+                "Rocket League is a fun game.", Arrays.asList("Huis", "Koelkast", "Broodrooster"), sogeti);
+        tournament(TournamentType.OFFICIAL, "HS", "images-optimized/activities/hs.jpg", "1 V 1", "Hearthstone",
+                "Hearthstone is not a fun game.", Arrays.asList("Keyboard", "Muis", "50 Packs"), coolerMaster);
+        tournament(TournamentType.UNOFFICIAL, "Achtung", "images-optimized/unofficial/achtung.jpg", "1 V 1",
+                "Achtung Die Kurve", "Achtung!", Arrays.asList("Kratje Hertog Jan", "Kratje Heineken", "Kratje Amstel"), null);
+        tournament(TournamentType.UNOFFICIAL, "JD", "images-optimized/unofficial/justdance.jpg", "2 V 2", "Just Dance",
+                "Just Dance is about dancing.", Collections.singletonList("Nothing."), sogeti);
+        //endregion Web Data
+    }
+
+    private CommitteeMember committeeMember(Long position, String function, String name, String icon) {
+        CommitteeMember committeeMember = new CommitteeMember();
+        committeeMember.setPosition(position);
+        committeeMember.setFunction(function);
+        committeeMember.setName(name);
+        committeeMember.setIcon(icon);
+        return committeeRepository.save(committeeMember);
+    }
+
+    private FaqPair faqpair(String question, String answer) {
+        FaqPair faqPair = new FaqPair();
+        faqPair.setQuestion(question);
+        faqPair.setAnswer(answer);
+        return faqRepository.save(faqPair);
+    }
+
+    private Sponsor sponsor(String name, SponsorType type, String website, String imageName) {
+        Sponsor sponsor = new Sponsor();
+        sponsor.setName(name);
+        sponsor.setType(type);
+        sponsor.setWebsite(website);
+        sponsor.setImageName(imageName);
+        return sponsorRepository.save(sponsor);
+    }
+
+    private Tournament tournament(TournamentType type, String buttonTitle, String buttonImagePath, String format,
+                                  String headerTitle, String description, List<String> prizes, Sponsor sponsor) {
+        Tournament tournament = new Tournament();
+        tournament.setType(type);
+        tournament.setButtonTitle(buttonTitle);
+        tournament.setButtonImagePath(buttonImagePath);
+        tournament.setFormat(format);
+        tournament.setHeaderTitle(headerTitle);
+        tournament.setDescription(description);
+        tournament.setPrizes(prizes);
+        tournament.setSponsor(sponsor);
+        return tournamentRepository.save(tournament);
     }
 }
