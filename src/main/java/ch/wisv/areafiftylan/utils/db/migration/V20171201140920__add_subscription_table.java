c512ab90555b5af7606636556ca22d97cbe640c9
Jurriaan Den Toonder <jur.den.toonder@gmail.com>
2017-12-05 14:50:55 +0100

    Fix naming convention for add subscription migration
    Signed-off-by: Jurriaan Den Toonder <jur.den.toonder@gmail.com>

--- src/main/java/ch/wisv/areafiftylan/utils/db/migration/V20171201140920__add_subscription_table.java
+++ src/main/java/ch/wisv/areafiftylan/utils/db/migration/V20171201140920__add_subscription_table.java
@@ -3,7 +3,7 @@ package ch.wisv.areafiftylan.utils.db.migration;
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-public class V20171201140920_add_subscription_table implements SpringJdbcMigration {
+public class V20171201140920__add_subscription_table implements SpringJdbcMigration {
     @Override
     public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
         jdbcTemplate.execute("CREATE TABLE \"subscription\" (\"id\" BIGINT, \"email\" VARCHAR(255) UNIQUE, PRIMARY KEY (id));");
