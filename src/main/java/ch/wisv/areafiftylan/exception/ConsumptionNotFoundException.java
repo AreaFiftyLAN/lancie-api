449a9d4c55a57cb7529ee1478204a83ae2327bda
Jurriaan Den Toonder <jur.den.toonder@gmail.com>
2018-01-08 22:30:29 +0100

    Change years from 2016 to 2018 in copyrights

--- src/main/java/ch/wisv/areafiftylan/exception/ConsumptionNotFoundException.java
+++ src/main/java/ch/wisv/areafiftylan/exception/ConsumptionNotFoundException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016  W.I.S.V. 'Christiaan Huygens'
+ * Copyright (c) 2018  W.I.S.V. 'Christiaan Huygens'
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
