221de347857e50f5d98787537cee4eba3a7d7743
Julian van Dijk <j.m.c.vandijk@student.tudelft.nl>
2020-02-15 14:22:30 +0100

    disable slack notifications during testing

--- src/main/java/ch/wisv/areafiftylan/utils/SlackNotificationAspect.java
+++ src/main/java/ch/wisv/areafiftylan/utils/SlackNotificationAspect.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 
 @Aspect
 @Component
-@Profile("!development")
+@Profile("!development && !test")
 public class SlackNotificationAspect {
 
     private Slack slackApi;
